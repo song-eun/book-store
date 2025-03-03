2fea200c21149eda5fd89c22d9290f3223222245
song_eun <80393294+song-eun@users.noreply.github.com>
2025-03-03 17:11:14 +0900

    feat: Header 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Header />
+      <hr></hr>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">Book Store</div>
+      <div className="search-box">
+        <input className="search-input" type="text" placeholder="Search" />
+        <button className="search-btn">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            size="lg"
+            className="white-icon"
+          />
+        </button>
+      </div>
+
+      <div className="customer-service">
+        <Link to="#home">Sign up</Link>
+        <Link to="#home">Log in</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
